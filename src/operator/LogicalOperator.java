e2ff6d177f3ff52eef134e5e4d7e8b05c3c3dedc
prachi <prachi3084@gmail.com>
2025-06-11 12:43:01 +0530

    Test commit

---
+++ src/operator/LogicalOperator.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        boolean a = true;
+        System.out.println(!a);
+
+        boolean b = true;
+        System.out.println(!!b);
+
+        boolean c= true || false;
+
+        System.out.println(c);
+
+
+        boolean c1= true && false;
+        System.out.println(c1);
+
+    }
+}
